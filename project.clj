ce767a8138dc9a61038b637f27018826585ac866
Aaron-Signer <mrboetarted@yahoo.com>
2018-09-30 14:03:44 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject event-tracker "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.7.0"]
+                 [compojure "1.6.1"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler event-tracker.core/main-handler}
+  :main ^:skip-aot event-tracker.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
